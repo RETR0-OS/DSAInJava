1a738d14776d633ad42e8b6b620fffe6531cb9c9
RETR0-OS <aadityajindal12@gmail.com>
2025-02-16 01:08:44 -0700

    use diamond operator

--- Recursion/LinearSearch.java
+++ Recursion/LinearSearch.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 class LinearSearch{
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,4,5,7,9,100};
-        ArrayList<Integer> ans = findAll(new ArrayList<Integer>(),arr, 4, 0);
+        ArrayList<Integer> ans = findAll(new ArrayList<>() ,arr, 4, 0);
         for(int a:ans){
             System.out.print(a + " ");
         }
